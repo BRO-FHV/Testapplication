2fb5dedacf34b0448792533a0144971381323588
Johannes Schwendinger <johannes.schwendinger@hotmail.com>
2014-06-11 11:03:59 +0200

    Kommentare entfernt

--- main.c
+++ main.c
@@ -1,16 +1,16 @@
-/*
+
 #include <Console/lib_console.h>
 #include <inttypes.h>
-
+/*
  * main.c
  */
 void main() {
-	/*
+
 	uint8_t i = 0;
 	while(i < 1000){
 		i = i + 1;
-		//lib_print("New int: %d", i);
+		lib_print("New int: %d", i);
 	}
-	*/
+
 	while(1){}
 }
